3bea158b6fc007fe0a3e050780ceed8f120d37f2
SIDD58 <siddharthsamber94@gmail.com>
2026-02-24 10:00:35 -0500

    fragment and parent child

--- index.jsx
+++ index.jsx
@@ -1,9 +1,44 @@
 import {createRoot} from 'react-dom/client'
+import { Fragment } from 'react'
 
 const root=createRoot(document.getElementById('root'))
-root.render(<h1>Hello</h1>)
-console.log("React Element Javascript object")
-console.log(<h1>Hello</h1>)
-const h1=document.createElement('h1')
-console.log("Actual browser DOM element")
-console.log(h1)
+root.render(
+    <>
+    <Header/>
+    <MainContent/>
+    <Footer/>
+    </>
+)
+function Header()
+{
+    return(
+        <>
+        <header>
+            <h1>Header component , this is independent custom component</h1>
+        </header>
+        </>
+    )
+}
+function MainContent()
+{
+    return(
+        <Fragment>
+            <ol>
+                <li>Fragment Component does not add extra div</li>
+                <li>Hence it does not break CSS layout styles , parent child realtionship of 
+                    elements like table - tr - td  </li>
+                <li>It does not add any extra DOM node like Div</li>
+            </ol>
+            <p> Fragment Component has shorthand syntax which we use for our header</p>
+        </Fragment>
+    )
+}
+
+function Footer()
+{
+    return(
+        <small>Copyright</small>
+    )
+
+
+}
