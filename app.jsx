f4619685af070a58f28c929319a9426edb406e96
SIDD58 <siddharthsamber94@gmail.com>
2026-02-25 15:34:16 -0500

    added CSS styling to header and entry

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Entry from './components/Entry'
+
+export default function App()
+{
+    return(
+        <>
+        <Header></Header>
+        <Entry></Entry>
+
+        </>
+    )
+}
--- index.jsx
+++ index.jsx
@@ -1,9 +1,4 @@
 import {createRoot} from 'react-dom/client'
-
+import App from './app'
 const root=createRoot(document.getElementById('root'))
-root.render(<h1>Hello</h1>)
-console.log("React Element Javascript object")
-console.log(<h1>Hello</h1>)
-const h1=document.createElement('h1')
-console.log("Actual browser DOM element")
-console.log(h1)
+root.render(<App/>)
