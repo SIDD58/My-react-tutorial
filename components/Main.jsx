0a01bcac9ff09737a38dbd799f245cf589030b8b
SIDD58 <siddharthsamber94@gmail.com>
2026-02-26 18:46:56 -0500

    spread operator list

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 export default function Main()
 {
-    const [ingredients,setIngredients]=useState(['onions','tomatoes','peas'])
+    const [ingredients,setIngredients]=useState([])
     const ingredients_list_items=ingredients.map(
     (ingredient)=> <li key={ingredient}>{ingredient}</li>)
     const handle_submit= (event)=> {
@@ -10,12 +10,8 @@ export default function Main()
         const form_data=new FormData(event.currentTarget)
         if (form_data.has('ingredient')){
             let ingredient_value=form_data.get('ingredient')
-            ingredients.push(ingredient_value)
-            console.log(ingredient_value)
-            console.log(ingredients)
-            setIngredients(ingredients)
-        }
-        
+            setIngredients((prevState)=>[...prevState,ingredient_value])
+        } 
     }
     return (
          <main>
