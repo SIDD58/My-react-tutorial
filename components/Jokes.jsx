3451eb9b82c3a98cafd5ca74eea4d37d93e7f965
SIDD58 <siddharthsamber94@gmail.com>
2026-02-27 08:44:20 -0500

    ternary operator added

--- components/Jokes.jsx
+++ components/Jokes.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 export default function({setup,punchline,count})
 {
     const [isShown,setIsShown]=useState(false)
+    const [unreadMessages, setUnreadMessages] = useState([])
 
     console.log(isShown)
     console.log(setup,punchline)
@@ -9,11 +10,13 @@ export default function({setup,punchline,count})
         <>
         <div className="joke-card">
             {/* conditonal rendering */}
-            {setup && <p>Setup: {setup}</p>}
-            {isShown && <p>Punchline: {punchline}</p>}
+            {setup ? <p>Setup: {setup}</p>:null}
+            {isShown ? <p>Punchline: {punchline}</p>:null}
             <p>Count: {count}</p>
-            <button onClick={()=>{setIsShown(!isShown)}}>Show Punchline</button>
+            <button onClick={()=>{setIsShown(!isShown)}}>{isShown?"Hide":"Show"} Punchline</button>
         </div>
+        {unreadMessages.length != 0 && <h1>You have {unreadMessages.length} unread messages!</h1> }
+        {unreadMessages.length == 0 && <p>You have no messages to read</p>}
         </>
     )
 }
