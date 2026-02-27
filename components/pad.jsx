9541806c16708beaac3c9101701eab211ffb2fb5
SIDD58 <siddharthsamber94@gmail.com>
2026-02-27 16:15:11 -0500

    Creating state in child also and onClick(wrong way)

--- components/pad.jsx
+++ components/pad.jsx
@@ -1,8 +1,10 @@
+import {useState} from 'react'
 export default function Pad({color,on_status})
 {
+    const [on_stat,setOnStat]=useState(on_status)
     return (
         (
-            <button style={{backgroundColor:color}} className={on_status?"on":""}></button>)
+            <button style={{backgroundColor:color}} className={on_stat?"on":""} onClick={()=>{setOnStat(!on_stat)}}></button>)
         )
         
 }
