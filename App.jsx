5f168c48d34324407f7e2187bc8df8c5f2bb07aa
SIDD58 <siddharthsamber94@gmail.com>
2026-02-27 17:15:48 -0500

    right way passing paramter from child to parent and then using id to update state

--- App.jsx
+++ App.jsx
@@ -5,8 +5,28 @@ import Pad from './components/Pad'
 export default function App({darkMode}) {
     const [pad_buttons,setPads]=useState(pads)
     const state_change=(id)=>{
-       console.log(id)
-    }
+    console.log(id)
+        setPads(
+        (prevStat)=>{
+            const pad_intial_state=prevStat[id-1]
+            pad_intial_state.on=!pad_intial_state.on
+            const newStat=prevStat.map(
+                (state_object)=>{
+                    if(state_object.id == pad_intial_state.id)
+                    {
+                        return pad_intial_state
+                    }
+                    else
+                    {
+                        return state_object
+                    } 
+                }
+            )
+            return (
+                newStat
+            )
+        }
+    )}
     const pad_list=pad_buttons.map((pad)=><Pad key={pad.id} color={pad.color} on_status={pad.on} id={pad.id} pad_click={state_change}/>)
     return (
         <main>
@@ -16,3 +36,6 @@ export default function App({darkMode}) {
         </main>
     )
 }
+
+
+
