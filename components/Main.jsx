82c6ce8d8ddac839c025293540947e7c00a7c8b9
SIDD58 <siddharthsamber94@gmail.com>
2026-02-27 06:18:46 -0500

    cleint side form handling react 19 added

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,10 +4,7 @@ export default function Main()
     const [ingredients,setIngredients]=useState([])
     const ingredients_list_items=ingredients.map(
     (ingredient)=> <li key={ingredient}>{ingredient}</li>)
-    const handle_submit= (event)=> {
-        event.preventDefault()
-        console.log('Ok I am clicked')
-        const form_data=new FormData(event.currentTarget)
+    const handle_submit= (form_data)=> {
         if (form_data.has('ingredient')){
             let ingredient_value=form_data.get('ingredient')
             setIngredients((prevState)=>[...prevState,ingredient_value])
@@ -15,7 +12,7 @@ export default function Main()
     }
     return (
          <main>
-            <form action="" className="add-ingredient-form" onSubmit={handle_submit}>
+            <form action={handle_submit} className="add-ingredient-form" >
                 <input type="text" aria-label="Add ingredients" name="ingredient" id="" placeholder="Eg. Onions"/>
                 <button>+ Add Ingredients</button>
             </form>
