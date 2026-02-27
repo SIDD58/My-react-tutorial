378e15a846a15e29e3ad50714732144603296ccd
SIDD58 <siddharthsamber94@gmail.com>
2026-02-27 08:18:42 -0500

    conditional rendering 1

--- components/Jokes.jsx
+++ components/Jokes.jsx
@@ -1,13 +1,18 @@
+import {useState} from 'react'
 export default function({setup,punchline,count})
 {
+    const [isShown,setIsShown]=useState(false)
+
+    console.log(isShown)
     console.log(setup,punchline)
     return (
         <>
         <div className="joke-card">
             {/* conditonal rendering */}
             {setup && <p>Setup: {setup}</p>}
-            <p>Punchline: {punchline}</p>
+            {isShown && <p>Punchline: {punchline}</p>}
             <p>Count: {count}</p>
+            <button onClick={()=>{setIsShown(!isShown)}}>Show Punchline</button>
         </div>
         </>
     )
